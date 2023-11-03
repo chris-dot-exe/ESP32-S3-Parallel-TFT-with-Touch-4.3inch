4feb023f045114d128dfd3dd46e9bfb145a344aa
Christian Exenberger <ce@stilgrenze.de>
2023-11-03 23:39:54 +0100

    Updated Examples to Arduino GFX Library v1.3.8

--- examples/Clock/Clock.ino
+++ examples/Clock/Clock.ino
@@ -5,22 +5,25 @@ Hardware:2.0
 Date 2023/4/17
 */
 
+/*******************************************************************************
+ * Start of Arduino_GFX setting
+
+ * If lib isn't downloaded by IDE add the library as a zip:
+ * https://github.com/moononournation/Arduino_GFX/archive/refs/tags/v1.3.8.zip
+ ******************************************************************************/
 #include <Arduino_GFX_Library.h>
 
-Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-    GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
     40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
     45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
     5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
-    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
-);
+    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */,
+    0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
+    0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
+    1 /* pclk_active_neg */, 16000000 /* prefer_speed */);
 
-// Uncomment for ST7262 IPS LCD 800x480
- Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
-   bus,
-   800 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
-   480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
-   1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+    800 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */);
 /*******************************************************************************
  * End of Arduino_GFX setting
  ******************************************************************************/
--- examples/LvglWidgets/LvglWidgets.ino
+++ examples/LvglWidgets/LvglWidgets.ino
@@ -1,3 +1,11 @@
+#include <Arduino_DataBus.h>
+#include <Arduino_G.h>
+#include <Arduino_GFX.h>
+#include <Arduino_GFX_Library.h>
+#include <Arduino_TFT.h>
+#include <Arduino_TFT_18bit.h>
+#include <gfxfont.h>
+
 /*******************************************************************************
  * LVGL Widgets
  * This is a widgets demo for LVGL - Light and Versatile Graphics Library
@@ -26,6 +34,12 @@
  ******************************************************************************/
 #include "lv_demo_widgets.h"
 
+/*******************************************************************************
+ * Start of Arduino_GFX setting
+
+ * If lib isn't downloaded by IDE add the library as a zip:
+ * https://github.com/moononournation/Arduino_GFX/archive/refs/tags/v1.3.8.zip
+ ******************************************************************************/
 #include <Arduino_GFX_Library.h>
 
 #define GFX_BL DF_GFX_BL // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
@@ -35,24 +49,18 @@
 /* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
 
 #define GFX_BL 44
-Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-    GFX_NOT_DEFINED /* CS */, GFX_NOT_DEFINED /* SCK */, GFX_NOT_DEFINED /* SDA */,
+Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
     40 /* DE */, 41 /* VSYNC */, 39 /* HSYNC */, 42 /* PCLK */,
     45 /* R0 */, 48 /* R1 */, 47 /* R2 */, 21 /* R3 */, 14 /* R4 */,
     5 /* G0 */, 6 /* G1 */, 7 /* G2 */, 15 /* G3 */, 16 /* G4 */, 4 /* G5 */,
-    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */
-);
+    8 /* B0 */, 3 /* B1 */, 46 /* B2 */, 9 /* B3 */, 1 /* B4 */,
+    0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
+    0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
+    1 /* pclk_active_neg */, 16000000 /* prefer_speed */);
+    
+Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
+    800 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */);
 
-// Uncomment for ST7262 IPS LCD 800x480
-Arduino_RPi_DPI_RGBPanel *gfx = new Arduino_RPi_DPI_RGBPanel(
-    bus,
-    800 /* width */, 0 /* hsync_polarity */, 8 /* hsync_front_porch */, 4 /* hsync_pulse_width */, 8 /* hsync_back_porch */,
-    480 /* height */, 0 /* vsync_polarity */, 8 /* vsync_front_porch */, 4 /* vsync_pulse_width */, 8 /* vsync_back_porch */,
-    1 /* pclk_active_neg */, 16000000 /* prefer_speed */, true /* auto_flush */);
-
-/*******************************************************************************
- * End of Arduino_GFX setting
- ******************************************************************************/
 
 /*******************************************************************************
  * End of Arduino_GFX setting
